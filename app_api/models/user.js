9ed337dfcc4cd4d1076dee23b55175bca08cb3e0
Claudio Restifo <claudio@samuiweb.net>
2017-04-10 17:36:25 +0700

    finished user schema

--- app_api/models/user.js
+++ app_api/models/user.js
@@ -1,5 +1,6 @@
 var mongoose require('mongoose');
 var crypto require('crypto');
+var jwt require('jsonwebtoken');
 
 var userSchema = new mongoose.Schema({
   email: {
@@ -24,3 +25,17 @@ userSchema.methods.validPassword = function(password) {
   var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
   return this.hash === hash;
 };
+
+userSchema.methods.generateJwt = function(){
+ var expiry = new Date();
+ expiry.setDate(expiry.getDate() + 7);
+
+ return jwt.sign({
+  _id: this._id,
+  email: this.email,
+  name: this.name,
+  exp: parseInt(expiry.getTime() / 1000), 
+ }, "MY_SECRET "); //replace it with the env variable
+}
+
+mongoose.model('User', userSchema);
